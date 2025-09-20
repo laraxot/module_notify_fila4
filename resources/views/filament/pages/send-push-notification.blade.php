f963d2c0025f991d16df9c46380b9568539b020c
marco76tv <marco.sottana@gmail.com>
2025-09-20 23:20:48 +0200

    .

--- resources/views/filament/pages/send-push-notification.blade.php
+++ resources/views/filament/pages/send-push-notification.blade.php
@@ -17,6 +17,7 @@ declare(strict_types=1);
                 <div class="mt-6">
                     {{ $this->notificationForm }}
                 </div>
+<<<<<<< HEAD
 
                 <div class="mt-6 flex items-center justify-between gap-x-3">
                     <div>
@@ -33,6 +34,10 @@ declare(strict_types=1);
                             {{ $action }}
                         @endforeach
                     </div>
+=======
+                <div>
+                    <x-filament::actions :actions="$this->getNotificationFormActions()" />
+>>>>>>> 518c702 (.)
                 </div>
             </div>
         </div>
