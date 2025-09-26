579ba960fbf296a28ce7109a447e591504243324
marco76tv <marco.sottana@gmail.com>
2025-09-26 18:25:52 +0200

    .

--- app/Filament/Clusters/Test/Pages/SendEmailPage.php
+++ app/Filament/Clusters/Test/Pages/SendEmailPage.php
@@ -253,7 +253,7 @@ class SendEmailPage extends XotBasePage
 =======
 >>>>>>> 3f537838 (.)
 
-    protected static string|\BackedEnum|null $navigationIcon = 'heroicon-o-paper-airplane';
+    protected static string | \BackedEnum | null $navigationIcon = 'heroicon-o-paper-airplane';
 
     protected string $view = 'notify::filament.pages.send-email';
 
