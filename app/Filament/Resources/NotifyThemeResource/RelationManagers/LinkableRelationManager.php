bcdab8e8b6d89a104eabb044472230ad3b0bb38e
marco76tv <marco.sottana@gmail.com>
2025-11-11 10:51:35 +0100

    .

--- app/Filament/Resources/NotifyThemeResource/RelationManagers/LinkableRelationManager.php
+++ app/Filament/Resources/NotifyThemeResource/RelationManagers/LinkableRelationManager.php
@@ -5,6 +5,10 @@ declare(strict_types=1);
 namespace Modules\Notify\Filament\Resources\NotifyThemeResource\RelationManagers;
 
 use Filament\Forms\Components\TextInput;
+<<<<<<< HEAD
+=======
+use Filament\Schemas\Schema;
+>>>>>>> 6bd5430 (.)
 use Modules\Xot\Filament\Resources\RelationManagers\XotBaseRelationManager;
 
 class LinkableRelationManager extends XotBaseRelationManager
