038f5e8ce99633c5e15a8dbdd3c80d004a8920bd
marco76tv <marco.sottana@gmail.com>
2025-11-12 10:12:21 +0100

    .

--- app/Filament/Clusters/Test/Pages/SendFirebasePushNotificationPage.php
+++ app/Filament/Clusters/Test/Pages/SendFirebasePushNotificationPage.php
@@ -95,7 +95,16 @@ class SendFirebasePushNotificationPage extends XotBasePage
 
     protected function fillForms(): void
     {
+<<<<<<< HEAD
         // Form data filled;
+=======
+        $this->pushForm->fill();
+    }
+
+    public function pushForm(Schema $schema): Schema
+    {
+        return $schema->components($this->getPushFormSchema())->model($this->getUser())->statePath('pushData');
+>>>>>>> 6bd5430 (.)
     }
 
     /**
@@ -145,7 +154,11 @@ class SendFirebasePushNotificationPage extends XotBasePage
 
     public function sendPushNotification(): void
     {
+<<<<<<< HEAD
         $data = $this->data;
+=======
+        $data = $this->pushForm->getState();
+>>>>>>> 6bd5430 (.)
 
         try {
             // Creare i dati della notifica Firebase
