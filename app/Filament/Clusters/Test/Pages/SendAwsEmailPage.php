158085bea13dc485e8e66fde515d1a48306632d3
marco76tv <marco.sottana@gmail.com>
2025-11-12 11:32:14 +0100

    rebase 210

--- app/Filament/Clusters/Test/Pages/SendAwsEmailPage.php
+++ app/Filament/Clusters/Test/Pages/SendAwsEmailPage.php
@@ -27,6 +27,7 @@ class SendAwsEmailPage extends XotBasePage
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     protected static string|\BackedEnum|null $navigationIcon = 'heroicon-o-envelope';
 =======
     protected static string | \BackedEnum | null $navigationIcon = 'heroicon-o-envelope';
@@ -34,6 +35,9 @@ class SendAwsEmailPage extends XotBasePage
 =======
     protected static string|\BackedEnum|null $navigationIcon = 'heroicon-o-envelope';
 >>>>>>> c64cc83 (.)
+=======
+    protected static string | \BackedEnum | null $navigationIcon = 'heroicon-o-envelope';
+>>>>>>> 2c58290 (.)
 
     protected string $view = 'notify::filament.pages.send-email';
 
