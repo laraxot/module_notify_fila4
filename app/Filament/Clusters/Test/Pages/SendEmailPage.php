1a6e69c4180a80247c94b8bfaea28e2a175d3efa
marco76tv <marco.sottana@gmail.com>
2025-11-12 11:32:24 +0100

    rebase 210

--- app/Filament/Clusters/Test/Pages/SendEmailPage.php
+++ app/Filament/Clusters/Test/Pages/SendEmailPage.php
@@ -261,6 +261,7 @@ class SendEmailPage extends XotBasePage
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     protected static string|\BackedEnum|null $navigationIcon = 'heroicon-o-paper-airplane';
 =======
     protected static string | \BackedEnum | null $navigationIcon = 'heroicon-o-paper-airplane';
@@ -286,6 +287,9 @@ class SendEmailPage extends XotBasePage
 =======
     protected static string|\BackedEnum|null $navigationIcon = 'heroicon-o-paper-airplane';
 >>>>>>> d60d83a (.)
+=======
+    protected static string | \BackedEnum | null $navigationIcon = 'heroicon-o-paper-airplane';
+>>>>>>> e9f2f09 (.)
 
     protected string $view = 'notify::filament.pages.send-email';
 
