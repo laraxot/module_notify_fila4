7f48c2e14f244604067fffa1428de6f0dfe8d4b7
marco76tv <marco.sottana@gmail.com>
2025-11-12 11:31:47 +0100

    rebase 210

--- app/Filament/Clusters/Test/Pages/SendEmailPage.php
+++ app/Filament/Clusters/Test/Pages/SendEmailPage.php
@@ -354,10 +354,14 @@ class SendEmailPage extends XotBasePage
 >>>>>>> 3f537838 (.)
 
 <<<<<<< HEAD
+<<<<<<< HEAD
     protected static string|\BackedEnum|null $navigationIcon = 'heroicon-o-paper-airplane';
 =======
     protected static string | \BackedEnum | null $navigationIcon = 'heroicon-o-paper-airplane';
 >>>>>>> 99ff506 (.)
+=======
+    protected static string|\BackedEnum|null $navigationIcon = 'heroicon-o-paper-airplane';
+>>>>>>> d2d07aa (.)
 
     protected string $view = 'notify::filament.pages.send-email';
 
