4363de8500fbc80f96bd3b915d36f91237684b9d
marco76tv <marco.sottana@gmail.com>
2025-11-12 10:10:21 +0100

    .

--- app/Filament/Clusters/Test/Pages/SendNetfunSmsPage.php
+++ app/Filament/Clusters/Test/Pages/SendNetfunSmsPage.php
@@ -90,14 +90,6 @@ class SendNetfunSmsPage extends XotBasePage
         $this->smsForm->fill();
     }
 
-<<<<<<< HEAD
-=======
-    public function smsForm(Schema $schema): Schema
-    {
-        return $schema->components($this->getSmsFormSchema())->model($this->getUser())->statePath('smsData');
-    }
-
->>>>>>> 6bd5430 (.)
     public function getSmsFormSchema(): array
     {
         return [
