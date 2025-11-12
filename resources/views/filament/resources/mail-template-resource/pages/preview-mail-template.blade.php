d3de55cf87bc695042f6e4af902ee74a086f79aa
marco76tv <marco.sottana@gmail.com>
2025-11-12 10:09:41 +0100

    .

--- resources/views/filament/resources/mail-template-resource/pages/preview-mail-template.blade.php
+++ resources/views/filament/resources/mail-template-resource/pages/preview-mail-template.blade.php
@@ -37,6 +37,7 @@ declare(strict_types=1);
             <div class="mt-1">
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 <<<<<<< HEAD
                 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
@@ -49,16 +50,21 @@ declare(strict_types=1);
 =======
 =======
 >>>>>>> bf479cc (.)
+=======
+>>>>>>> 372da3e (.)
                 <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                     @foreach($this->record->variables as $key => $value)
                         <div class="p-2 bg-gray-50 rounded">
                             <span class="font-medium">{{ $key }}:</span>
                             <span class="text-gray-600">{{ $value }}</span>
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> 11ae5222 (.)
 >>>>>>> 62b0c9c (.)
 =======
 >>>>>>> bf479cc (.)
+=======
+>>>>>>> 372da3e (.)
                         </div>
                     @endforeach
                 </div>
