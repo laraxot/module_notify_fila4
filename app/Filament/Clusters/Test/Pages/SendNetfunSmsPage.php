33a71e3bef7ba0368988b5e12c53f247c05ef9b2
marco76tv <marco.sottana@gmail.com>
2025-11-12 11:31:47 +0100

    rebase 210

--- app/Filament/Clusters/Test/Pages/SendNetfunSmsPage.php
+++ app/Filament/Clusters/Test/Pages/SendNetfunSmsPage.php
@@ -181,6 +181,7 @@ class SendNetfunSmsPage extends XotBasePage
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     protected static string|\BackedEnum|null $navigationIcon = 'heroicon-o-device-phone-mobile';
 =======
     protected static string | \BackedEnum | null $navigationIcon = 'heroicon-o-device-phone-mobile';
@@ -188,6 +189,9 @@ class SendNetfunSmsPage extends XotBasePage
 =======
     protected static string|\BackedEnum|null $navigationIcon = 'heroicon-o-device-phone-mobile';
 >>>>>>> d2d07aa (.)
+=======
+    protected static string | \BackedEnum | null $navigationIcon = 'heroicon-o-device-phone-mobile';
+>>>>>>> b586b08 (.)
 
     protected string $view = 'notify::filament.pages.send-sms';
 
