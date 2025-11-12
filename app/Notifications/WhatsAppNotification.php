0dc0bc01d208636732cd9dbbcec6b816a7eedee0
marco76tv <marco.sottana@gmail.com>
2025-11-12 10:10:57 +0100

    .

--- app/Notifications/WhatsAppNotification.php
+++ app/Notifications/WhatsAppNotification.php
@@ -148,7 +148,7 @@ class WhatsAppNotification extends Notification implements ShouldQueue
     /**
      * Get the provider configuration for this notification.
      *
-     * @return array<int, string>
+     * @return array<string, mixed>
      */
     public function getConfig(): array
     {
