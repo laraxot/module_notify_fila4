2fb980c965302718f17bec6a0ddf22c2a72dddec
marco76tv <marco.sottana@gmail.com>
2025-12-09 17:50:10 +0100

    .

--- app/Notifications/SmsNotification.php
+++ app/Notifications/SmsNotification.php
@@ -1018,7 +1018,7 @@ class SmsNotification extends Notification implements ShouldQueue
     /**
      * Get the provider configuration for this notification.
      *
-     * @return array<int, string>
+     * @return array<string, mixed>
      */
     public function getConfig(): array
     {
--- app/Notifications/WhatsAppNotification.php
+++ app/Notifications/WhatsAppNotification.php
@@ -1381,7 +1381,7 @@ class WhatsAppNotification extends Notification implements ShouldQueue
     /**
      * Get the provider configuration for this notification.
      *
-     * @return array<int, string>
+     * @return array<string, mixed>
      */
     public function getConfig(): array
     {
