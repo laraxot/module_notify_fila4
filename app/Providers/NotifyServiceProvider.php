61f2f4f974eecaa1796a16b50091dd509216a7b2
marco76tv <marco.sottana@gmail.com>
2025-12-09 17:38:29 +0100

    .

--- app/Providers/NotifyServiceProvider.php
+++ app/Providers/NotifyServiceProvider.php
@@ -397,9 +397,11 @@ class NotifyServiceProvider extends XotBaseServiceProvider
 >>>>>>> cf20697a6 (.)
         $mail = TenantService::config('mail');
         Assert::isArray($mail);
-        $fallback_to = Arr::get($mail, 'fallback_to', null);
-        if (is_string($fallback_to)) {
-            Mail::alwaysTo($fallback_to);
+
+        $fallbackTo = Arr::get($mail, 'fallback_to');
+
+        if (is_string($fallbackTo)) {
+            Mail::alwaysTo($fallbackTo);
         }
 
         // }
