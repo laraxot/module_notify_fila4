6ff964b76c2204cfdc0c348d4326647771f9f174
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:09:24 +0100

    .

--- app/Filament/Clusters/Test/Pages/SendNetfunSmsPage.php
+++ app/Filament/Clusters/Test/Pages/SendNetfunSmsPage.php
@@ -41,6 +41,7 @@ class SendNetfunSmsPage extends XotBasePage
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     protected static string|\BackedEnum|null $navigationIcon = 'heroicon-o-device-phone-mobile';
 =======
     protected static string | \BackedEnum | null $navigationIcon = 'heroicon-o-device-phone-mobile';
@@ -75,6 +76,9 @@ class SendNetfunSmsPage extends XotBasePage
 =======
     protected static string | \BackedEnum | null $navigationIcon = 'heroicon-o-device-phone-mobile';
 >>>>>>> 6ded0f9 (.)
+=======
+    protected static string|\BackedEnum|null $navigationIcon = 'heroicon-o-device-phone-mobile';
+>>>>>>> ccb1541 (.)
 
     protected string $view = 'notify::filament.pages.send-sms';
 
