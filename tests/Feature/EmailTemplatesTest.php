58f9c40a739a3df4ddfe9b4a50a0bd4890571d3e
marco76tv <marco.sottana@gmail.com>
2025-12-10 00:48:06 +0100

    .

--- tests/Feature/EmailTemplatesTest.php
+++ tests/Feature/EmailTemplatesTest.php
@@ -789,8 +789,8 @@ test('html template contains optional function', function (): void {
 =======
 >>>>>>> d5d0864 (.)
 
-use Tests\TestCase;
 use Illuminate\Support\Facades\File;
+use Tests\TestCase;
 
 uses(TestCase::class);
 
