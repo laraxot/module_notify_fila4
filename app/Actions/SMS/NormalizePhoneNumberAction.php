39d94758a9e38042b3fac670fbfffbefbb3bd2d4
marco76tv <marco.sottana@gmail.com>
2025-12-09 17:02:18 +0100

    .

--- app/Actions/SMS/NormalizePhoneNumberAction.php
+++ app/Actions/SMS/NormalizePhoneNumberAction.php
@@ -114,6 +114,7 @@ class NormalizePhoneNumberAction
 {
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     public function execute(string|array $phoneNumber): string
     {
         // Convert array to string if needed
@@ -129,6 +130,10 @@ class NormalizePhoneNumberAction
     public function execute(string $phoneNumber): string
     {
 >>>>>>> bf479cc (.)
+=======
+    public function execute(string $phoneNumber): string
+    {
+>>>>>>> 372da3e (.)
         // Rimuove parentesi e il loro contenuto
 <<<<<<< HEAD
 <<<<<<< HEAD
--- resources/views/filament/pages/send-push-notification.blade.php
+++ resources/views/filament/pages/send-push-notification.blade.php
@@ -71,6 +71,7 @@ declare(strict_types=1);
                 </div>
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
                 <div>
                     <x-filament::actions :actions="$this->getNotificationFormActions()" />
 =======
@@ -80,6 +81,8 @@ declare(strict_types=1);
 =======
 =======
 >>>>>>> bf479cc (.)
+=======
+>>>>>>> 372da3e (.)
 
                 <div class="mt-6 flex items-center justify-between gap-x-3">
                     <div>
@@ -97,10 +100,13 @@ declare(strict_types=1);
                         @endforeach
                     </div>
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> 11ae5222 (.)
 >>>>>>> 62b0c9c (.)
 =======
 >>>>>>> bf479cc (.)
+=======
+>>>>>>> 372da3e (.)
                 </div>
             </div>
         </div>
