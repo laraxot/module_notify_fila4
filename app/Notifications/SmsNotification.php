ac3615d5df877a52c87c2e8b6e26a5a9e0e4c646
marco76tv <marco.sottana@gmail.com>
2025-12-09 17:41:56 +0100

    .

--- app/Notifications/SmsNotification.php
+++ app/Notifications/SmsNotification.php
@@ -704,7 +704,7 @@ class SmsNotification extends Notification implements ShouldQueue
     /**
      * Get the provider configuration for this notification.
      *
-     * @return array<int, string>
+     * @return array<string, mixed>
      */
     public function getConfig(): array
     {
