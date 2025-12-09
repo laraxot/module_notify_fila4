3981e054b1513aa6d72f7354b1b734aaf0fca5fa
marco76tv <marco.sottana@gmail.com>
2025-12-09 17:22:24 +0100

    .

--- app/Filament/Resources/NotifyThemeResource/RelationManagers/LinkableRelationManager.php
+++ app/Filament/Resources/NotifyThemeResource/RelationManagers/LinkableRelationManager.php
@@ -62,6 +62,10 @@ namespace Modules\Notify\Filament\Resources\NotifyThemeResource\RelationManagers
 >>>>>>> 4689a827 (.)
 use Override;
 use Filament\Forms\Components\TextInput;
+<<<<<<< HEAD
+=======
+use Filament\Schemas\Schema;
+>>>>>>> b8140d8 (.)
 use Modules\Xot\Filament\Resources\RelationManagers\XotBaseRelationManager;
 
 <<<<<<< HEAD
