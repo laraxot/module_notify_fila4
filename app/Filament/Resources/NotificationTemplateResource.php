b6650658daa264ed0ca9910ed17f267f7176825b
marco76tv <marco.sottana@gmail.com>
2025-12-09 17:32:20 +0100

    .

--- app/Filament/Resources/NotificationTemplateResource.php
+++ app/Filament/Resources/NotificationTemplateResource.php
@@ -756,6 +756,10 @@ use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
 use Modules\Notify\Filament\Resources\NotificationTemplateResource\Pages\PreviewNotificationTemplate;
 use Dotswan\FilamentGrapesjs\Forms\Components\Grapesjs;
+<<<<<<< HEAD
+=======
+use Filament\Forms;
+>>>>>>> 62b0c9c (.)
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
