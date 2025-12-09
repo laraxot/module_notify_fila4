3e2abc5944ef049c0e4af166da223a8c2a836a08
marco76tv <marco.sottana@gmail.com>
2025-12-09 17:17:51 +0100

    .

--- app/Actions/SMS/NormalizePhoneNumberAction.php
+++ app/Actions/SMS/NormalizePhoneNumberAction.php
@@ -231,6 +231,7 @@ class NormalizePhoneNumberAction
 {
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     public function execute(string|array $phoneNumber): string
     {
         // Convert array to string if needed
@@ -246,6 +247,10 @@ class NormalizePhoneNumberAction
     public function execute(string $phoneNumber): string
     {
 >>>>>>> bf479cc (.)
+=======
+    public function execute(string $phoneNumber): string
+    {
+>>>>>>> 372da3e (.)
         // Rimuove parentesi e il loro contenuto
 <<<<<<< HEAD
 <<<<<<< HEAD
