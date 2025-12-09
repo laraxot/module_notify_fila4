30c01a2329a5e4be3f00d3322ee90866f5e6f068
marco76tv <marco.sottana@gmail.com>
2025-12-10 00:56:42 +0100

    .

--- app/Filament/Resources/NotifyThemeResource/RelationManagers/LinkableRelationManager.php
+++ app/Filament/Resources/NotifyThemeResource/RelationManagers/LinkableRelationManager.php
@@ -1205,7 +1205,7 @@ class LinkableRelationManager extends XotBaseRelationManager
     public function getFormSchema(): array
     {
         return [
-            TextInput::make('id')
+            'id' => TextInput::make('id')
                 ->required()
                 ->maxLength(255),
 <<<<<<< HEAD
