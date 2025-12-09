81b922820f691a6a0fbaf62dc8fa24ccd2a28153
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:13:29 +0100

    .

--- database/factories/NotificationLogFactory.php
+++ database/factories/NotificationLogFactory.php
@@ -5,19 +5,34 @@ declare(strict_types=1);
 namespace Modules\Notify\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+use Modules\Notify\Enums\NotificationLogStatusEnum;
+use Modules\Notify\Models\NotificationLog;
 
+/**
+ * @extends Factory<NotificationLog>
+ */
 class NotificationLogFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     */
-    protected $model = \Modules\Notify\Models\NotificationLog::class;
+    protected $model = NotificationLog::class;
 
-    /**
-     * Define the model's default state.
-     */
     public function definition(): array
     {
-        return [];
+        /** @var NotificationLogStatusEnum $status */
+        $status = $this->faker->randomElement(NotificationLogStatusEnum::cases());
+
+        return [
+            'notifiable_type' => 'App\\Models\\User',
+            'notifiable_id' => $this->faker->numberBetween(1, 1000),
+            'title' => $this->faker->sentence(),
+            'content' => $this->faker->paragraph(),
+            'channels' => ['mail'],
+            'data' => [
+                'tracking_id' => $this->faker->uuid(),
+            ],
+            'sent_at' => Carbon::now()->subMinutes($this->faker->numberBetween(1, 120)),
+            'status' => $status->value,
+            'error' => null,
+        ];
     }
 }
