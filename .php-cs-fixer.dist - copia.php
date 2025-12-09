1ed281c7b9f0645028bb89f97a977bba708ee7fd
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:04:11 +0100

    .

--- .php-cs-fixer.dist - copia.php
+++ .php-cs-fixer.dist - copia.php
@@ -70,6 +70,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> 82ae73be (.)
 =======
@@ -891,6 +892,11 @@ declare(strict_types=1);
 >>>>>>> 098285fb (rebase 210)
 =======
 >>>>>>> e42e3233 (rebase 210)
+=======
+declare(strict_types=1);
+
+
+>>>>>>> e120248f (.)
 $finder = PhpCsFixer\Finder::create()
     ->notPath('bootstrap/cache')
     ->notPath('storage')
