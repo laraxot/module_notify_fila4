3dc8f4aeae9f2f512f9a2ded137623219f0cb720
marco76tv <marco.sottana@gmail.com>
2025-12-10 01:06:12 +0100

    .

--- app/Filament/Clusters/Test/Pages/SendAwsEmailPage.php
+++ app/Filament/Clusters/Test/Pages/SendAwsEmailPage.php
@@ -134,6 +134,7 @@ class SendAwsEmailPage extends XotBasePage
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     protected static string|\BackedEnum|null $navigationIcon = 'heroicon-o-envelope';
 =======
     protected static string | \BackedEnum | null $navigationIcon = 'heroicon-o-envelope';
@@ -141,6 +142,9 @@ class SendAwsEmailPage extends XotBasePage
 =======
     protected static string|\BackedEnum|null $navigationIcon = 'heroicon-o-envelope';
 >>>>>>> c64cc83 (.)
+=======
+    protected static string | \BackedEnum | null $navigationIcon = 'heroicon-o-envelope';
+>>>>>>> 2c58290 (.)
 
     protected string $view = 'notify::filament.pages.send-email';
 
