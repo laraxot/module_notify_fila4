d7cfc50bfaed222980fbacc87cba0bc5d418c464
marco76tv <marco.sottana@gmail.com>
2025-12-10 12:03:58 +0100

    .

--- app/Notifications/TicketAssignedNotification.php
+++ app/Notifications/TicketAssignedNotification.php
@@ -35,7 +35,7 @@ class TicketAssignedNotification extends Notification
     }
 
     /**
-     * @return array<int, string>
+     * @return array<string, mixed>
      */
     public function toArray(mixed $notifiable): array
     {
--- app/Notifications/WhatsAppNotification.php
+++ app/Notifications/WhatsAppNotification.php
@@ -3343,7 +3343,7 @@ class WhatsAppNotification extends Notification implements ShouldQueue
     /**
      * Get the provider configuration for this notification.
      *
-     * @return array<int, string>
+     * @return array<string, mixed>
      */
     public function getConfig(): array
     {
