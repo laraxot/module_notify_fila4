764ad76ca2e09051620be7b4eb3d4f3e86ace7b5
marco76tv <marco.sottana@gmail.com>
2025-12-10 19:59:40 +0100

    .

--- app/Services/PushNotificationService.php
+++ app/Services/PushNotificationService.php
@@ -443,6 +443,7 @@ class PushNotificationService
         if (strlen($token) === 64 && ctype_xdigit($token)) {
             return 'apns';
 <<<<<<< HEAD
+<<<<<<< HEAD
         }
         if (strlen($token) > 100 && str_contains($token, ':')) {
             return 'fcm';
@@ -456,6 +457,14 @@ class PushNotificationService
             return 'webpush';
         }
 >>>>>>> 8bc2fc9f (first)
+=======
+        }
+        if (strlen($token) > 100 && str_contains($token, ':')) {
+            return 'fcm';
+        }
+
+        return 'webpush';
+>>>>>>> 20a3d3b (.)
     }
 
     /**
