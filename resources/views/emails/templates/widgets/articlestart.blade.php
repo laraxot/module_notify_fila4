9df29d70ecd34ee95a287430ea5c11251cb99020
marco76tv <marco.sottana@gmail.com>
2025-12-23 13:23:22 +0100

    .

--- resources/views/emails/templates/widgets/articlestart.blade.php
+++ resources/views/emails/templates/widgets/articlestart.blade.php
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
-e width="100%" cellpadding="0" cellspacing="0" border="0">
-=======
 <?php
 
 declare(strict_types=1);
 
 ?>
 <table width="100%" cellpadding="0" cellspacing="0" border="0">
->>>>>>> 518c702 (.)
 	<tr>
 		<td bgcolor="{{ isset($color) ? $color : '#ef3101' }}" nowrap>
 			<img border="0" src="{{ Request::getSchemeAndHttpHost() }}/img/spacer.gif" width="5" height="1">
