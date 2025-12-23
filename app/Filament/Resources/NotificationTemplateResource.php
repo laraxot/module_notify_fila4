2df01eceadd5fdaf04e743bc51ce0f06784ab770
marco76tv <marco.sottana@gmail.com>
2025-12-23 13:26:39 +0100

    .

--- app/Filament/Resources/NotificationTemplateResource.php
+++ app/Filament/Resources/NotificationTemplateResource.php
@@ -23,45 +23,73 @@ class NotificationTemplateResource extends XotBaseResource
     public static function getFormSchema(): array
     {
         return [
+<<<<<<< HEAD
             'name' => TextInput::make('name')
+=======
+            TextInput::make('name')
+>>>>>>> b8140d8 (.)
                 ->required()
                 ->maxLength(255)
                 ->unique(ignoreRecord: true)
                 ->helperText(__('notify::template.form.name.helper'))
                 ->columnSpan(['lg' => 2]),
+<<<<<<< HEAD
             'subject' => TextInput::make('subject')
+=======
+            TextInput::make('subject')
+>>>>>>> b8140d8 (.)
                 ->required()
                 ->maxLength(255)
                 ->helperText(__('notify::template.form.subject.helper'))
                 ->columnSpan(['lg' => 2])
                 ->translateLabel(),
+<<<<<<< HEAD
             'type' => Select::make('type')
+=======
+            Select::make('type')
+>>>>>>> b8140d8 (.)
                 ->options(collect(NotificationTypeEnum::cases())
                     ->mapWithKeys(fn ($type) => [$type->value => $type->label()]))
                 ->required()
                 ->default(NotificationTypeEnum::EMAIL->value)
                 ->helperText(__('notify::template.form.type.helper'))
                 ->columnSpan(['lg' => 1]),
+<<<<<<< HEAD
             'body_text' => Textarea::make('body_text')
+=======
+            Textarea::make('body_text')
+>>>>>>> b8140d8 (.)
                 ->required()
                 ->maxLength(65535)
                 ->columnSpan(['lg' => 3])
                 ->helperText(__('notify::template.form.body_text.helper'))
                 ->rows(5)
                 ->translateLabel(),
+<<<<<<< HEAD
             'body_html' => Textarea::make('body_html')
+=======
+            Textarea::make('body_html')
+>>>>>>> b8140d8 (.)
                 ->required()
                 ->maxLength(65535)
                 ->columnSpan(['lg' => 3])
                 ->helperText(__('notify::template.form.body_html.helper'))
                 ->rows(10)
                 ->translateLabel(),
+<<<<<<< HEAD
             'preview_data' => Textarea::make('preview_data')
+=======
+            Textarea::make('preview_data')
+>>>>>>> b8140d8 (.)
                 ->json()
                 ->columnSpan(['lg' => 3])
                 ->helperText(__('notify::template.form.preview_data.helper'))
                 ->rows(5),
+<<<<<<< HEAD
             'attachments' => SpatieMediaLibraryFileUpload::make('attachments')
+=======
+            SpatieMediaLibraryFileUpload::make('attachments')
+>>>>>>> b8140d8 (.)
                 ->collection('attachments')
                 ->multiple()
                 ->maxFiles(5)
