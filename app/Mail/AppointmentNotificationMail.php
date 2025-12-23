3415acbc20e1cfb09b4779bdaab374c7a43fdcc5
marco76tv <marco.sottana@gmail.com>
2025-12-23 13:39:41 +0100

    .

--- app/Mail/AppointmentNotificationMail.php
+++ app/Mail/AppointmentNotificationMail.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace Modules\Notify\Mail;
 
-use Illuminate\Mail\Mailables\Attachment;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Attachment;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
