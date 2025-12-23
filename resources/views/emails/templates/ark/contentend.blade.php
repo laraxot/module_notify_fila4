56592b0b59293e79e1b17b617800d846ec97e09e
marco76tv <marco.sottana@gmail.com>
2025-12-23 13:37:29 +0100

    .

--- resources/views/emails/templates/ark/contentend.blade.php
+++ resources/views/emails/templates/ark/contentend.blade.php
@@ -1,9 +1,13 @@
+<<<<<<< HEAD
 <?php
 
 declare(strict_types=1);
 
 ?>
 </div>
+=======
+>
+>>>>>>> 0db165c (.)
 					</td>
 				</tr>
 			</tbody>
@@ -13,4 +17,8 @@ declare(strict_types=1);
 </tr>
 <tr>
 	<td colspan="3" height="30"></td>
+<<<<<<< HEAD
 </tr>
+=======
+</tr>
+>>>>>>> 0db165c (.)
