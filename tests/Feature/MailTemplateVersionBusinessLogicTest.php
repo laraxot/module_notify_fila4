60b7e086190057df5a6d330a7c300eabc6cbc107
marco76tv <marco.sottana@gmail.com>
2025-12-23 13:40:01 +0100

    .

--- tests/Feature/MailTemplateVersionBusinessLogicTest.php
+++ tests/Feature/MailTemplateVersionBusinessLogicTest.php
@@ -342,7 +342,7 @@ class MailTemplateVersionBusinessLogicTest extends TestCase
 
         foreach ($mailableClasses as $index => $mailableClass) {
             /** @var \Illuminate\Database\Eloquent\Collection */
-        $version = MailTemplateVersion/** @phpstan-ignore-line */ ::factory()->create([
+            $version = MailTemplateVersion/** @phpstan-ignore-line */ ::factory()->create([
                 'template_id' => $template->id,
                 'mailable' => $mailableClass,
                 'version' => '1.'.$index,
@@ -466,7 +466,7 @@ class MailTemplateVersionBusinessLogicTest extends TestCase
 
         foreach ($versionSchemes as $versionNumber => $description) {
             /** @var \Illuminate\Database\Eloquent\Collection */
-        $version = MailTemplateVersion/** @phpstan-ignore-line */ ::factory()->create([
+            $version = MailTemplateVersion/** @phpstan-ignore-line */ ::factory()->create([
                 'template_id' => $template->id,
                 'version' => $versionNumber,
                 'change_notes' => $description,
