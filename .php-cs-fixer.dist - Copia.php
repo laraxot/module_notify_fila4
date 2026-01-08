2da63f56f63c58be4b34fc62f1b9f6b7cb63bed5
marco76tv <marco.sottana@gmail.com>
2026-01-08 12:45:23 +0100

    .

--- .php-cs-fixer.dist - Copia.php
+++ .php-cs-fixer.dist - Copia.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+
 $finder = PhpCsFixer\Finder::create()
     ->notPath('bootstrap/cache')
     ->notPath('storage')
@@ -10,7 +11,7 @@ $finder = PhpCsFixer\Finder::create()
     ->name('*.php')
     ->notName('*.blade.php')
     ->ignoreDotFiles(true)
-    ->ignoreVCS(true);
+    ->ignoreVCS(true)
 
 $config = new PhpCsFixer\Config();
 
@@ -37,6 +38,6 @@ $config
         'class_definition' => true,
         'elseif' => true,
     ])
-    ->setFinder($finder);
+    ->setFinder($finder)
 
 return $config;
