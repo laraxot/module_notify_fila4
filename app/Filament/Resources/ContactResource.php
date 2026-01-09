bb17cf4e6bb3efdbd5db421c91735474b411e3bb
marco76tv <marco.sottana@gmail.com>
2026-01-09 12:46:22 +0100

    .

--- app/Filament/Resources/ContactResource.php
+++ app/Filament/Resources/ContactResource.php
@@ -14,8 +14,6 @@ class ContactResource extends XotBaseResource
 {
     protected static ?string $model = Contact::class;
 
-    protected static string | \BackedEnum | null $navigationIcon = 'heroicon-o-rectangle-stack';
-
     /**
      * Get the form schema for the resource.
      *
